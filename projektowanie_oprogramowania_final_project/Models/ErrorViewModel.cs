7b7267a75eedf91422379467c356adcc6c2c539c
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2022-12-17 14:01:22 +0100

    Init

---
+++ projektowanie_oprogramowania_final_project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projektowanie_oprogramowania_final_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
