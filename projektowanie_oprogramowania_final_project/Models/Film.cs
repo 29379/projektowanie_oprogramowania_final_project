b0456e4c07b0176a24ba84e7e9776b09c450ed12
Beata Kowalewicz <bea.kowalewicz@gmail.com>
2022-12-18 21:58:29 +0100

    Add models

---
+++ projektowanie_oprogramowania_final_project/Models/Film.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace projektowanie_oprogramowania_final_project.Models
+{
+    public class Film
+    {
+        [Key]
+        public int FilmId { get; set; }
+
+        [Required]
+        public string Tytul { get; set; }
+
+        [Required]
+        public string Rezyser { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DataWydania { get; set; }
+
+        public string Opis { get; set; }
+
+        [Required]
+        public TimeSpan CzasTrwania { get; set; }
+    }
+}
---
+++ projektowanie_oprogramowania_final_project/Models/Rezerwacja.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace projektowanie_oprogramowania_final_project.Models
+{
+    public class Rezerwacja
+    {
+        [Key]
+        public int RezerwacjaId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime DataGodzina { get; set; }
+
+        [Required]
+        public Double Koszt { get; set; }
+    }
+}
