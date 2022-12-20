f6d8a297360f37343fa5bed412d547962807f7b2
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2022-12-20 14:41:49 +0100

    ???

---
+++ projektowanie_oprogramowania_final_project/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Employee
+{
+
+    public enum Position
+    {
+        admin,
+        common
+    }
+
+    public class Employee : User.User
+    {
+        [Required]
+        public Position Role { get; set; }
+
+        public Employee(int id, string name, string surname, string email, string phoneNumber, string password, Position role)
+        {
+            UserId = id;
+            Name = name;
+            Surname = surname;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Password = password;
+            Role = role;
+        }
+
+        public Employee(int id, string name, string surname, string email, string password, Position role)
+            : this(id, name, surname, email, null, password, role) { }
+
+        public Employee(int id, string name, string surname, string email, string phoneNumber, string password)
+            : this(id, name, surname, email, phoneNumber, password, Position.common) { }
+
+        public Employee(int id, string name, string surname, string email, string password)
+            : this(id, name, surname, email, null, password, Position.common) { }
+    }
+}
