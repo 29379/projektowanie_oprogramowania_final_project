2e86a4330e6198a0c966b23d9dfa85a017cfbccd
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2023-01-03 16:29:53 +0100

    Modified few models (to be reviewed). First migration done

--- projektowanie_oprogramowania_final_project/Models/Seat.cs
+++ projektowanie_oprogramowania_final_project/Models/Seat.cs
@@ -18,16 +18,7 @@ namespace projektowanie_oprogramowania_final_project.Models
             Przy takiej logice jeśli ktoś kupi bilet na siedzenie np 9
             w danej sali na wtorek 17.00, to dopóki seans nie przeminie, 
             to to siedzenie będzie 'occupied' i nie będzie można
-            kupić biletu np na środę. Poza tym, jak jest C#, to czy te
-            obiekty będą kopiowane? Czy referencja/pointer? Powiedzmy że
-            działa to już inaczej, nieważne jak - jest kilka różnych rezerwacji
-            na ten sam film - kiedy nowa rezerwacja bierze obiekt 'siedzenia', to ona
-            będzie musiała się tego złapać po referencji, bo jeśli będzie po wartości
-            to nowa rezerwacja nie będzie widziała że miejsce jest zajęte
-            bo wewnątrz każdej rezerwacji będą nowe kopie obiektów siedzeń,
-            zamiast referencji do jednego, tego samego obiektu z bazy danych. Mam wrażenie że
-            się powtarzam, mam nadzieję że wypowiedź ma sens, ale nie wiem jak to
-            będzie działało w C# do końca więc nie wiem co z tym zrobić.  */
+            kupić biletu np na środę.   */
         [Required]
         public bool Occupied { get; set; }
     }
